1f9abdb9feb7a04ec8aee75b47d9f2b6e8236d55
dannyel-34 <dcalle338@gmail.com>
2015-06-12 01:13:48 +0000

    guardar modificaciones

--- app/helpers/profesors_helper.rb
+++ app/helpers/profesors_helper.rb
@@ -1,8 +1,8 @@
 module ProfesorsHelper
     
-  def calcular_edad(fch_nacimiento)
+    def calcular_edad(fch_nacimiento)
       now = Date.today
       year = now.year - fch_nacimiento.year
-  end
+    end
   
 end
--- app/models/sucursal.rb
+++ app/models/sucursal.rb
@@ -1,3 +1,21 @@
 class Sucursal < ActiveRecord::Base
   belongs_to :institucion
+  
+  #valida campos obligatorios
+  validates :cod, :presence => true
+  validates :sede, :presence => true
+  validates :direccion, :presence => true
+  validates :email, :presence => true
+  
+   # validar formato de un email 
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: 'Formato no valido' }
+  
+  # valida la longitud de un campo, tanto la longitud minima como la maxima 
+  validates :cod, length: { minimum: 5, maximum: 15 } 
+  validates :direccion, length: { minimum: 5, maximum: 45 }
+  validates :sede, length: { minimum: 5, maximum: 45 } 
+  
+  # Valida si un atributo es numerico 
+  validates :cod, numericality: true
+  
 end
